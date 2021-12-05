606dc1f945fc228f9768b935ce8931f2d65f450c
Aleksandr Baran <lidanko12@gmail.com>
2021-12-05 22:47:45 +0200

    add

--- src/components/BadSearch/BadSearch.js
+++ src/components/BadSearch/BadSearch.js
@@ -2,7 +2,7 @@ import s from './BadSearch.module.css'
 export default function BadSearch() {
     return (
         <div className={s.container}>
-            <h3 className={s.error}>Please enter searching repository !
+            <h3 className={s.error}>Nothing to display. Use search above to find some repos
                 <div className={s.back}></div></h3>
    </div> )
     
